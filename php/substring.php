08998fd7748a256ae5a028cc7a2a1785ab2ca0f3
ephekt <mfrosengarten@gmail.com>
2012-05-09 15:43:50 -0700

    clean up and simplify substring function

--- php/substring.php
+++ php/substring.php
@@ -1,37 +1,30 @@
 <?php
 
 function substring($haystack,$needle) {
-	if("" == $needle) { return true; }
-	echo "Haystack:\n$haystack\n";
-    echo "Needle:\n$needle\n";
-	
-	for($i=0,$len=strlen($haystack);$i<$len;$i++){
-		if($needle[0] == $haystack[$i]) {
-			$found = true;
-			for($j=0,$slen=strlen($needle);$j<$slen;$j++) {
-				if($j >= $len) {
-					echo "reached end of haystack but have more needles";
-					return false;
-				}
-				if($needle[$j] != $haystack[$i+$j]) {
-					//echo "Mismatch, restart\n";
-					$found = false;
-					continue;
-				}
+	if("" == $needle) { return 0; }
+	echo "Haystack: $haystack\nNeedle: $needle\n";
+	$h_length = strlen($haystack);
+	$n_length = strlen($needle);	
+	for( $i=0; ($h_length - $i) >= $n_length; $i++){
+		$found = true;
+		for($j=0;$j<$n_length;$j++) {
+			if($needle[$j] != $haystack[$i+$j]) {
+				$found = false;
+				continue;
 			}
-			if($found) {
-				echo " . . . . . . SUCCESS!!!! startPos: $i\n";
-				return true;
-			}
+		}
+		if($found) {
+			echo " . . . . . . SUCCESS!!!! startPos: $i\n";
+			return $i;
 		}
 	}
 	echo " . . . . . . FAILURE!\n" ;
-	return false;
+	return -1;
 }
 
-assert(substring("haystack","hay"));
-assert(!substring("ack","hoy"));
-assert(substring("hayhayhay","hayhay"));
-assert(substring("mucho22","22"));
-assert(!substring("str","string"));
+substring("haystack","hay");
+!substring("ack","hoy");
+substring("hayhayhay","hayhay");
+substring("mucho22","22");
+!substring("str","string");
 ?>
